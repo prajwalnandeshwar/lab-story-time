a499e2a6deac85c104b23ec9e88c24f8da48d1ef
prajwalnandeshwar <prajwaljn1008@gmail.com>
2021-09-11 18:15:24 +0530

    Commit Message

--- src/app.js
+++ src/app.js
@@ -2,15 +2,34 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+moreAboutHome =(address, distanceFromTown, hasNeighbours) => {
+    let all = typeof(address)+typeof(distanceFromTown)+typeof(hasNeighbours)
+    return all;
+}
 
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+moreAboutKaren = (parents, noOfSiblings, isNuclearFamily) =>{
+    if(typeof(parents)=='string'&&typeof(noOfSiblings)=='number'&&typeof(isNuclearFamily)=='boolean')
+    return true;
+    else
+    return false;
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+doesFriendExist = (ageInText, ageInNumber) => {
+    if(ageInText==='NaN'){
+        return "NaN";
+    }
+    if(isNaN(ageInNumber)){
+        return ageInNumber;
+    }
+    return NaN;
+}
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -18,10 +37,28 @@
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
+sweetTooth =(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters,metersToTravel) =>{
+    totalNoOfSweets-=sweetsConsumedByKaren;
+    var n = metersToTravel*sweetsConsumedInNMeters;
+    if(n<=totalNoOfSweets)
+    {
+     totalNoOfSweets-=n;
+     return totalNoOfSweets/2;
+    }
+    else{
+    return "No sweets for Karen's friend";
+    }
+   }
 
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+convertToCelsius = (fahrenheit) =>{
+    if(typeof(fahrenheit)=='number')
+    return (fahrenheit-32) * (5/9);
+    else
+    return "Technical Error!";
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -30,6 +67,24 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+aDifficultChoice = (choice) =>{
+    switch(choice){
+    case 1:
+        choice = "Take her daughter to a doctor";
+        break;
+    case -1:
+        choice = "Break down and give up all hope";
+        break;
+    case undefined:
+        choice = "Wasn't able to decide";
+        break;
+    case 'I give up':
+        choice = "Refused to do anything for Karen";
+        break;
+
+    }
+    return choice;
+}
 
 //Progression 7:
 //Lily realized that she'd hurt her daughter
